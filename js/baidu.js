6fb5c79bbe1b3a5c2c9119dd3124d6e829aff9d0
simonkuang <me@simonkuang.com>
2019-06-10 13:39:11 +0800

    add baidu tongji

---
+++ js/baidu.js
@@ -0,0 +1,12 @@
+;;(function() {
+  // from Baidu Tongji
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?cf1cc6b1a5e225ee72f7361d4ad12298";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+
+  //document.addEventListener('DOMContentLoaded', function() {});
+})();
